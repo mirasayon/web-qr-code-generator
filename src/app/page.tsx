047d217aa21000020a6e3219ed9a5904b18e5fd5
MiraSayon <mirasayon@ya.ru>
2025-05-20 02:30:58 +0500

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,55 @@
+"use client";
 import Image from "next/image";
-
+import QRCode from "react-qr-code";
+import * as htmlToImage from "html-to-image";
+import { useRef } from "react";
+import type React from "react";
+import { toPng } from "html-to-image";
+import { useSearchParams } from "next/navigation";
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	const qrRef = useRef<HTMLDivElement>(null);
+	const query = useSearchParams().get("qrvalue");
+	const handleDownload = async () => {
+		if (!qrRef.current) return;
+		try {
+			const dataUrl = await toPng(qrRef.current);
+			const link = document.createElement("a");
+			link.download = "qr-code.png";
+			link.href = dataUrl;
+			link.click();
+		} catch (err) {
+			console.error("Error generating image", err);
+		}
+	};
+	return (
+		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+			<div>
+				<form
+					onSubmit={(asd) => {
+						console.log(asd);
+					}}
+					className=" p-2 border-amber-100 border-2"
+				>
+					<input type="text" name="qrvalue" id="qrvalue" className="text-4xl outline-none" />
+					<input type="submit" value="Create" className=" bg-green-200 text-black" />
+				</form>
+			</div>
+			<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+				<div className=" bg-black p-2 m-2 border-16 border-red-950">
+					{query ? (
+						<div>
+							<div ref={qrRef}>
+								<QRCode value={query} size={256} />
+							</div>
+							<button type="button" className=" cursor-pointer p-4" onClick={handleDownload}>
+								Download QR Code as PNG
+							</button>
+						</div>
+					) : (
+						<div>None</div>
+					)}
+				</div>
+			</main>
+		</div>
+	);
 }
